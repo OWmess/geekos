ed962cf5cd479320e8c2189c54422475a5384cb7
OWmess <1162739554@qq.com>
2023-03-05 16:24:04 +0800

    push

---
+++ requirement/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo '在接下来的步骤中，根据界面提示按y或enter继续安装.'
+echo '将在三秒后开始安装依赖'
+sleep 3
+sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+
+sudo apt install build-essential
+sudo apt install bochs,bochs-x,libc6-dev-i386
+
+tar xvzf nasm-2.08rc2.tar.gz
+cd ./nasm-2.08rc2
+
+./configure
+
+make 
+sudo make install
