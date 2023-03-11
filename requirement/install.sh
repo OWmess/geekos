bd9dc9eea1880d43f9429c37087f4e9849c32b7f
owmess <1162739554@qq.com>
2023-03-11 23:34:18 +0800

    update install.sh

--- requirement/install.sh
+++ requirement/install.sh
@@ -7,7 +7,7 @@ sleep 3
 sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 
 sudo apt install build-essential
-sudo apt install bochs,bochs-x,libc6-dev-i386
+sudo apt install bochs bochs-x libc6-dev-i386
 
 tar xvzf nasm-2.08rc2.tar.gz
 cd ./nasm-2.08rc2
